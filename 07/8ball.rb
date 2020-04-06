40420aad3d8ee14a8f1359c8c963e760a6dfc4f5
kopo4eweb <kopo4e.web@yandex.ru>
2020-04-06 18:17:58 +0300

    add hello text to magic 8 ball

---
+++ 07/8ball.rb
@@ -0,0 +1,32 @@
+answers = [
+    # Положительные
+    ["Бесспорно", "It is certain"],
+    ["Предрешено", "It is decidedly so"],
+    ["Никаких сомнений", "Without a doubt"],
+    ["Определённо да", "Yes — definitely"],
+    ["Можешь быть уверен в этом", "You may rely on it"],
+
+    # Нерешительно положительные
+    ["Мне кажется — «да»", "As I see it, yes"],
+    ["Вероятнее всего", "Most likely"],
+    ["Хорошие перспективы", "Outlook good"],
+    ["Знаки говорят — «да»", "Signs point to yes"],
+    ["Да", "Yes"],
+
+    # Нейтральные
+    ["Пока не ясно, попробуй снова", "Reply hazy, try again"],
+    ["Спроси позже", "Ask again later"],
+    ["Лучше не рассказывать", "Better not tell you now"],
+    ["Сейчас нельзя предсказать", "Cannot predict now"],
+    ["Сконцентрируйся и спроси опять", "Concentrate and ask again"],
+
+    # Отрицательные
+    ["Даже не думай", "Don’t count on it"],
+    ["Мой ответ — «нет»", "My reply is no"],
+    ["По моим данным — «нет»", "My sources say no"],
+    ["Перспективы не очень хорошие", "Outlook not so good"],
+    ["Весьма сомнительно", "Very doubtful"]
+]
+
+puts "Привет, дорогой друг. Отвечаю на твой вопрос...\n\r"
+puts answers.sample
